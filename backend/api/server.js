408bf2bfe2bfe91be16e1e069f14186379e2a69d
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-08-31 11:33:38 +0500

    port

--- backend/api/server.js
+++ backend/api/server.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: true,
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST'],
     credentials: true
   },
@@ -33,7 +33,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: true,
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 app.use(express.json());
