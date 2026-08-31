6f3ef7a160a5f16950222a4f798c17736c9d6fe1
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-08-31 12:42:31 +0500

    socket.io

--- backend/api/server.js
+++ backend/api/server.js
@@ -28,6 +28,7 @@ const io = new Server(server, {
     methods: ['GET', 'POST'],
     credentials: true
   },
+  transports: ['polling', 'websocket'],
   maxHttpBufferSize: 1e7 // 10MB for large payloads if needed
 });
 
