d2d518c5a0e7f6f691fc455007d66960763c3759
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-08-31 11:06:59 +0500

    msg

--- backend/api/server.js
+++ backend/api/server.js
@@ -223,7 +223,3 @@ io.on('connection', async (socket) => {
   });
 });
 
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
